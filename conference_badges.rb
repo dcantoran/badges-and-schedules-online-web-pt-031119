c34d379e7e440fdc5c95d2b36bbf084a587b211b
David Cantoran <davidcantoran36@gmail.com>
2019-03-22 23:03:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,8 +9,8 @@ def batch_badge_creator(attendees)
 end
 
 def assign_rooms(attendees) 
-    return attendees.map.with_index do |name, index| 
-      "Hello, #{name}! You'll be assigned to room #{index+1}!" 
+  attendees.map.with_index do |name, index| 
+    "Hello, #{name}! You'll be assigned to room #{index+1}!" 
   end
 end
 
